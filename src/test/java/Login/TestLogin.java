5c84bba69b446971488c5345a5fb414a2e31bc0a
VINAYA_DESAI <emaiid>
2018-01-30 21:56:22 -0800

    added new Test for invalid user

--- src/test/java/Login/TestLogin.java
+++ src/test/java/Login/TestLogin.java
@@ -4,10 +4,17 @@ import org.testng.annotations.Test;
 
 public class TestLogin {
 	
-	@Test
+	@Test(priority = 1)
 	void ValidUser()
 	{
 		System.out.println("hello from TestLogin ");
 	}
+	
+	
+	@Test(priority = 2)
+	void InValidUser()
+	{
+		System.out.println("I'm invalid user for TestLogin ");
+	}	
 
 }
